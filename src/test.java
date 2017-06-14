365147b8e4f02c4298c91e5c75f34cd89567b947
Anthony <anthonymendez9@gmail.com>
2017-06-14 13:08:12 -0400

    Created Repository with associating files.

---
+++ src/test.java
@@ -0,0 +1,58 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class test {
+
+	public static void main(String[] args) throws SecurityException, IOException {
+		ArrayList<Long> forwardRuns = new ArrayList<Long>();
+		ArrayList<Long> backwardRuns = new ArrayList<Long>();
+		int length = 100000;
+		int arr[] = new int[length];
+		long currTime, diffTime;
+		int runLimit = 10000000;
+		
+		PrintWriter forwardWrite = new PrintWriter("ForwardRuns.txt","UTF-8"),
+					backwardWrite = new PrintWriter("BackwardRuns.txt","UTF-8");
+		
+		Logger log = Logger.getLogger("MyLog");
+		FileHandler file = new FileHandler("LoopLog.log");
+		log.addHandler(file);
+		SimpleFormatter sm = new SimpleFormatter();
+		file.setFormatter(sm);
+		log.info(String.format("Loop Test | Runs: %d | Array Length: %d", runLimit, length));
+		
+		for(int run = 0; run < runLimit; run++){
+			currTime = System.nanoTime();
+			for(int i = 0; i < length; i++){
+				arr[i] = i;
+			}
+			diffTime = System.nanoTime()-currTime;
+			forwardRuns.add(diffTime);
+			forwardWrite.println(diffTime);
+			
+			String forward = String.format("Run: %d | Forward Loop Nano Time: %d", run+1,diffTime);
+			log.info(forward);
+//			System.out.println(forward);
+			
+			currTime = System.nanoTime();
+			for(int i = length-1; i >= 0; i--){
+				arr[i] = i;
+			}
+			diffTime = System.nanoTime()-currTime;
+			backwardRuns.add(diffTime);
+			backwardWrite.println(diffTime);
+			
+			String backward = String.format("Run: %d | Backwards Loop Nano Time: %d", run+1,diffTime);
+			log.info(backward);
+//			System.out.println(backward);
+		}
+		log.info("End of Test");
+		forwardWrite.close();
+		backwardWrite.close();
+	}
+
+}
